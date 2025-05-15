24b400452bb8eaee4817a08541bcfcf422c4f4a1
Keith Chason <kchason@users.noreply.github.com>
2025-05-15 13:56:28 +0000

    Update logic for CodeQuality export

--- pkg/reporting/exporters/codequality/codequality.go
+++ pkg/reporting/exporters/codequality/codequality.go
@@ -1,32 +1,32 @@
 package codequality
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"encoding/json"
 	"os"
 	"sync"
 
+	"github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/nuclei/v2/pkg/output"
+
 	"github.com/pkg/errors"
-	"github.com/projectdiscovery/nuclei/v3/pkg/output"
 )
 
-// CodeQualityRecord is based off the required fields from https://docs.gitlab.com/ci/testing/code_quality/#code-quality-report-format
-type CodeQualityRecord struct {
+// Record is based off the required fields from https://docs.gitlab.com/ci/testing/code_quality/#code-quality-report-format
+type Record struct {
 	description string
-	check_name  string
+	checkName   string `yaml:"check_name"`
 	fingerprint string
 	severity    string
-	location    struct {
-		path  string
-		lines struct {
-			begin int
-		}
-	}
+	path        string `yaml:"location.path"`
+	lineNumber  int    `yaml:"location.lines.begin"`
 }
 
 type Exporter struct {
 	options *Options
 	mutex   *sync.Mutex
-	rows    []CodeQualityRecord
+	rows    []Record
 }
 
 // Options contains the configuration options for CodeQuality exporter client
@@ -37,12 +37,25 @@ type Options struct {
 	File string `yaml:"file"`
 }
 
+func GenerateFingerprint(event *output.ResultEvent) (string, error) {
+	// Generates a UUID for the unique fingerprint
+	data, err := json.Marshal(event)
+	if err != nil {
+		return "", err
+	}
+
+	hasher := sha1.New()
+	hasher.Write(data)
+	sum := hasher.Sum(nil)
+	return hex.EncodeToString(sum), nil
+}
+
 // New creates a new exporter integration client based on options.
 func New(options *Options) (*Exporter, error) {
 	exporter := &Exporter{
 		mutex:   &sync.Mutex{},
 		options: options,
-		rows:    []CodeQualityRecord{},
+		rows:    []Record{},
 	}
 	return exporter, nil
 }
@@ -54,16 +67,24 @@ func (exporter *Exporter) Export(event *output.ResultEvent) error {
 	defer exporter.mutex.Unlock()
 
 	// Create a CodeQuality struct from the event
-	record := CodeQualityRecord{
+	lineNumber := 1
+	if len(event.Lines) > 0 {
+		lineNumber = event.Lines[0]
+	}
+
+	fingerprint, err := GenerateFingerprint(event)
+	if err != nil {
+		gologger.Warning().Msgf("failed to generate fingerprint, falling back to placeholder: %s", err)
+		fingerprint = "Nuclei Scan Result"
+	}
+
+	record := Record{
 		description: event.Info.Description,
-		check_name: event.Info.Name,
-		location: struct {
-			path: event.Url,
-			line: struct {
-				begin: 1
-			},
-		},
-		severity: event.Info.Severity,
+		checkName:   event.Info.Name,
+		fingerprint: fingerprint,
+		severity:    event.Info.SeverityHolder.Severity.String(),
+		path:        event.Path,
+		lineNumber:  lineNumber,
 	}
 
 	// Add the event to the rows
